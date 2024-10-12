8209393875fe768805ddeca38c56fb39e0ddf02e
James John McGuire <jamesjohnmcguire@gmail.com>
2024-10-12 16:14:55 +0900

    Add Agent Option for Interactive Mode

--- Commands/server.cmd
+++ Commands/server.cmd
@@ -1,8 +1,14 @@
 @ECHO OFF
+SET Mode=batch
 SET ServerUser=%1
 SET Authorization=%2 %3
 SET Port=%4
 SET command="%~5"
+
+IF NOT [%6]==[] SET Mode=agent
+
+
 ECHO command is: %command%
 
-CALL plink.exe -v -ssh -t -batch %ServerUser% %Authorization% -P %Port% %command%
+@ECHO ON
+plink.exe -v -ssh -t -%Mode% %ServerUser% %Authorization% -P %Port% %command%
