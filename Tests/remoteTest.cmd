7bb803338c285ee9990d1ccbd0e2c5e5aa61e34c
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-26 19:45:13 +0900

    Add Tests

---
+++ Tests/remoteTest.cmd
@@ -0,0 +1,30 @@
+@ECHO OFF
+SETLOCAL
+
+CD %~dp0
+CD ..\Commands
+
+SET testServer=%EuroCasaServer%
+SET testUser=%EuroCasaUserId%
+SET testRemotePath=/home/%EuroCasaUserId%
+SET testAuthentication=-pw %EuroCasaPassword%
+
+ECHO Getting file...
+CALL remote.cmd get %testServer% %testUser% %testAuthentication% %testRemotePath% .bash_history verbose
+DEL /Q .bash_history
+ECHO .
+
+ECHO Getting directory...
+CALL remote.cmd get %testServer% %testUser% %testAuthentication% %testRemotePath% bin verbose recurse
+RD /S /Q bin
+ECHO .
+
+ECHO Putting file...
+CALL remote.cmd put %testServer% %testUser% %testAuthentication% %testRemotePath% AdbReset.cmd verbose
+ECHO .
+
+ECHO Putting directory...
+CALL remote.cmd put %testServer% %testUser% %testAuthentication% %testRemotePath% profiles verbose recurse
+ECHO .
+
+ENDLOCAL
