4e57dfcf489a7658066971a12239ca82b44d56c7
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-30 21:11:30 +0900

    Update Tests

--- Tests/remoteTest.cmd
+++ Tests/remoteTest.cmd
@@ -9,27 +9,31 @@ SET testUser=%EuroCasaUserId%
 SET testRemotePath=/home/%EuroCasaUserId%
 SET testAuthentication=-pw %EuroCasaPassword%
 
+:get-file
 ECHO Getting file...
 CALL remote.cmd get %testServer% %testUser% %testAuthentication% %testRemotePath% .bash_history verbose
 DEL /Q .bash_history
 ECHO .
 
+:get-directory
 ECHO Getting directory...
 CALL remote.cmd get %testServer% %testUser% %testAuthentication% %testRemotePath% bin verbose recurse
 RD /S /Q bin
 ECHO .
 
+:get-database
 ECHO Getting database dump...
 CALL ServerDatabaseExport.cmd full %testServer% %testUser% %testAuthentication% %testUser% %EuroCasaDbPassword% %EuroCasaDbHost% %testUser%_main /home/%testUser% verbose
 DEL /Q %testUser%_main.full.sql
 ECHO .
 
 ECHO Putting file...
-CALL remote.cmd put %testServer% %testUser% %testAuthentication% %testRemotePath% AdbReset.cmd verbose
+CALL remote.cmd put %testServer% %testUser% %testAuthentication% %testRemotePath% test.sql verbose
 ECHO .
 
 ECHO Putting directory...
-CALL remote.cmd put %testServer% %testUser% %testAuthentication% %testRemotePath% profiles verbose recurse
+CALL remote.cmd put %testServer% %testUser% %testAuthentication% %testRemotePath% TestData verbose recurse
 ECHO .
 
+:end
 ENDLOCAL
