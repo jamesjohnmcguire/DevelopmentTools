e0f25ce13344aaf3b911d80f8de9c8bf256ea61f
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-30 21:05:26 +0900

    Expand to Handle Quotes

--- Commands/remoteBaseOptions.cmd
+++ Commands/remoteBaseOptions.cmd
@@ -2,10 +2,11 @@ SET recurse=false
 SET verbose=false
 
 :process
-IF "%1"=="" GOTO :done
+::ECHO Processing argument: %1
+IF "%~1"=="" GOTO :done
 
-IF /i "%1"=="recurse" SET recurse=true
-IF /i "%1"=="verbose" SET verbose=true
+IF /i "%~1"=="recurse" SET recurse=true
+IF /i "%~1"=="verbose" SET verbose=true
 
 :: Shift arguments to bring the next one into %1
 shift
