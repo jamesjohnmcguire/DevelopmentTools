2a614b51aad52fe467f1a82903070f14348df4eb
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-30 21:13:35 +0900

    Add Remote Command Support

--- Commands/remote.cmd
+++ Commands/remote.cmd
@@ -5,10 +5,8 @@ SET command=%1
 SET remoteServer=%2
 SET remoteUser=%3
 SET remoteAuthentication=%4 %5
-SET remotePath=%6
-SET scpFile=%7
 
-CALL remoteBaseOptions.cmd %1 %2 %3 %4 %5 %6 %7 %8 %9
+CALL remoteBaseOptions.cmd %*
 
 IF NOT [%remotePort%]==[] SET remotePort=-P 22
 IF %recurse%==true SET remoteOptions=-r
@@ -23,20 +21,37 @@ ECHO Port Is: %remotePort%
 ECHO Remote Server Is: %remoteServer%
 ECHO Remote User Is: %remoteUser%
 ECHO Remote Authentication Is: %remoteAuthentication%
-ECHO Remote Path Is: %remotePath%
-ECHO File Is: %scpFile%
 
 :continue
+IF %command%==command GOTO command
 IF %command%==get GOTO get
 IF %command%==put GOTO put
 
+:command
+SET remoteCommand=%6
+ECHO command is: [96m%remoteCommand%[0m
+
+:: plink.exe -v
+REM plink.exe -ssh -t -%Mode% %ServerUser% %Authorization% %remoteCommand%
+GOTO end
+
 :get
+SET remotePath=%6
+SET scpFile=%7
+IF %verbose%==true ECHO Remote Path Is: %remotePath%
+IF %verbose%==true ECHO File Is: %scpFile%
+
 IF %verbose%==true ECHO Command Is: pscp %remoteAuthentication% %remotePort% %remoteOptions% %remoteUser%@%remoteServer%:%remotePath%/%scpFile% %scpFile%
 
 pscp %remoteAuthentication% %remotePort% %remoteOptions% %remoteUser%@%remoteServer%:%remotePath%/%scpFile% %scpFile%
 GOTO end
 
 :put
+SET remotePath=%6
+SET scpFile=%7
+IF %verbose%==true ECHO Remote Path Is: %remotePath%
+IF %verbose%==true ECHO File Is: %scpFile%
+
 IF %verbose%==true ECHO Command Is: pscp %remoteAuthentication% %remotePort% %remoteOptions% %scpFile% %remoteUser%@%remoteServer%:%remotePath%
 
 pscp %remoteAuthentication% %remotePort% %remoteOptions% %scpFile% %remoteUser%@%remoteServer%:%remotePath%
