109abe94ce579974f725b6ee11684692b12801cd
James John McGuire <jamesjohnmcguire@gmail.com>
2025-10-03 18:02:51 +0900

    Add Error Level Checks

--- Tests/allTests.cmd
+++ Tests/allTests.cmd
@@ -10,10 +10,16 @@ CD ..\Commands
 ECHO Testing MySql Clean Up...
 CD TestData
 COPY /Y mysql-old-original.sql mysql-old-test.sql
+
 CALL mySqlClean.cmd mysql-old-test.sql
+
+SET checkCode=%ERRORLEVEL%
 DEL /Q mysql-old-test.sql
 CD ..
 ECHO .
+
+if %checkCode% NEQ 0 ECHO mySqlClean.cmd Check Failed
+if %checkCode% EQU 0 ECHO [96mmySqlClean.cmd Check Succeeded[0m
 
 :end
 ENDLOCAL
