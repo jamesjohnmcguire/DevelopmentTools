98217e2af4dc7f8f9055a30d851d24c33be974ac
James John McGuire <jamesjohnmcguire@gmail.com>
2025-11-10 16:30:05 +0900

    Fill Out Script

--- Commands/WpCf7Update.cmd
+++ Commands/WpCf7Update.cmd
@@ -1,25 +1,47 @@
+SETLOCAL
 CD %WebRoot%
 
-SET Id=%1
+"SET Id=%~1"
 
 :: File containing the form structure as HTML fragment
-SET Form=%2
+"SET Form=%~2"
+
+:: Should be in JSON format
+"SET Mail1Settings=%~3"
+"SET Mail2Settings=%~4"
 
 :: Text file with paired data like "key=value",
 :: Such as: flamingo_email: "[your-email]"
-SET AdditionalSettings=%3
+"SET AdditionalSettings=%~5"
 
-:: Should be in JSON format
-SET Mail1Settings=%4
-SET Mail2Settings=%5
+IF NOT EXIST "%Form%" SET ErrorMessage="Input file not found: %Form%"
+IF NOT EXIST "%FILE%" GOTO error
+IF NOT EXIST "%Mail1Settings%" SET ErrorMessage="Input file not found: %Mail1Settings%"
+IF NOT EXIST "%Mail1Settings%" GOTO error
 
 @ECHO ON
+CALL wp post update %Id% "%Form%"
+@ECHO ON
+CALL wp post meta update %Id% _form < "%Form%"
 
-CALL wp post update %Id% %2
 @ECHO ON
-CALL wp post meta update %Id% _form < %Form%
+CALL wp post meta update --format=json %Id% _mail < "%Mail1Settings%"
+
+:: Optional second mail settings file
+@IF "%Mail2Settings%"=="none" GOTO additional
+IF NOT EXIST "%Mail1Settings%" GOTO additional
+
 @ECHO ON
-CALL wp post meta update %Id% _additional_settings < %AdditionalSettings%
+CALL wp post meta update --format=json %Id% _mail_2 < "%Mail2Settings%"
+
+:additional
 @ECHO ON
-CALL wp post meta update --format=json %Id% _mail < %Mail1Settings%
-@ECHO ON
+IF "%~AdditionalSettings%"=="" GOTO finish
+CALL wp post meta update %Id% _additional_settings < "%AdditionalSettings%"
+
+:error
+SET LocalErrorLevel=1
+ECHO ERROR: %ErrorMessage%
+
+:finish
+ENDLOCAL & EXIT /B %LocalErrorLevel%
