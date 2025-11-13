883a36ffcfd23b3debdd2dd5db096c06d5a3e25c
James John McGuire <jamesjohnmcguire@gmail.com>
2025-11-13 17:24:32 +0900

    Use Correct Variable

--- Commands/ScpGet.cmd
+++ Commands/ScpGet.cmd
@@ -1,6 +1,7 @@
 @ECHO OFF
+
 SETLOCAL
-@if [%6]==[] GOTO usage
+::@if [%6]==[] GOTO usage
 
 SET server=%1
 SET user=%2
@@ -11,4 +12,4 @@ SET scpFile=%6
 IF NOT [%7]==[] SET option1=%7
 IF NOT [%8]==[] SET option2=%8
 
-CALL remote.cmd get %server% %user% %authentication% %remotePath% %file% %option1% %option2%
+CALL remote.cmd get %server% %user% %authentication% %remotePath% %scpFile% %option1% %option2%
