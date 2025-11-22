93c668085c3da459ddd7591740ba08297fb0cad2
James John McGuire <jamesjohnmcguire@gmail.com>
2025-11-22 19:37:37 +0900

    Clean Up Script

--- Commands/WpCf7Update.cmd
+++ Commands/WpCf7Update.cmd
@@ -1,26 +1,31 @@
 SETLOCAL
 CD %WebRoot%
 
-"SET Id=%~1"
+SET Id=%~1
+
+:: File containing the fields concatenated together
+SET Post=%~2
 
 :: File containing the form structure as HTML fragment
-"SET Form=%~2"
+SET Form=%~3
 
 :: Should be in JSON format
-"SET Mail1Settings=%~3"
-"SET Mail2Settings=%~4"
+SET Mail1Settings=%~4
+SET Mail2Settings=%~5
 
 :: Text file with paired data like "key=value",
 :: Such as: flamingo_email: "[your-email]"
-"SET AdditionalSettings=%~5"
+SET AdditionalSettings=%~5
 
+IF NOT EXIST "%Post%" SET ErrorMessage="Input file not found: %Post%"
+IF NOT EXIST "%Post%" GOTO error
 IF NOT EXIST "%Form%" SET ErrorMessage="Input file not found: %Form%"
-IF NOT EXIST "%FILE%" GOTO error
+IF NOT EXIST "%Form%" GOTO error
 IF NOT EXIST "%Mail1Settings%" SET ErrorMessage="Input file not found: %Mail1Settings%"
 IF NOT EXIST "%Mail1Settings%" GOTO error
 
 @ECHO ON
-CALL wp post update %Id% "%Form%"
+CALL wp post update %Id% "%Post%"
 @ECHO ON
 CALL wp post meta update %Id% _form < "%Form%"
 
@@ -36,8 +41,9 @@ CALL wp post meta update --format=json %Id% _mail_2 < "%Mail2Settings%"
 
 :additional
 @ECHO ON
-IF "%~AdditionalSettings%"=="" GOTO finish
+IF "%AdditionalSettings%"=="" GOTO finish
 CALL wp post meta update %Id% _additional_settings < "%AdditionalSettings%"
+GOTO finish
 
 :error
 SET LocalErrorLevel=1
